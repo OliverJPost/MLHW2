f2c117a55713608affeb6c778983823b5aba2623
Oliver J. Post <65900202+OliverJPost@users.noreply.github.com>
2023-03-27 00:41:34 +0200

    Iterate over all pointclouds and make boxplot

--- main.py
+++ main.py
@@ -1,8 +1,38 @@
+import numpy as np
+
 from pointcloud import PointCloud
+import pandas as pd
+import matplotlib.pyplot as plt
+
 
 def main():
-    cloud = PointCloud.from_file('data/pointclouds/000.xyz')
-    print(len(cloud))
+    labels = ["building", "car", "fence", "pole", "tree"]
+    horizontal_distribution_data = {label: [] for label in labels}
+    for hundred in range(5):
+        print(f"Processing {labels[hundred]}")
+        data = horizontal_distribution_data[labels[hundred]]
+        for number in range(100):
+            cloud = PointCloud.from_file(f"data/pointclouds/{hundred}{number:02d}.xyz")
+            evenness = cloud.horizontal_distribution_evenness()
+            data.append(evenness)
 
-if __name__ == '__main__':
+        print(f"Average {hundred}: {sum(data) / len(data)}")
+        print(f"Standard deviation: {hundred}: {np.std(data)}")
+        horizontal_distribution_data[labels[hundred]] = data
+
+    make_boxplot(
+        horizontal_distribution_data, "Horizontal Distribution Evenness by Class"
+    )
+
+
+def make_boxplot(all_data: dict[str, list[float]], title: str):
+    plt.boxplot(all_data.values())
+    plt.xticks(range(1, len(all_data) + 1), all_data.keys())
+    plt.xlabel("Class")
+    plt.ylabel("Return value")
+    plt.title(title)
+    plt.show()
+
+
+if __name__ == "__main__":
     main()
