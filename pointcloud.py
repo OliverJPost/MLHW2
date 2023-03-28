d1cdea538790ab01f178110bcbf709583be07ca4
rkh00 <33283775+rkh00@users.noreply.github.com>
2023-03-28 16:07:31 +0200

    Updated pointcloud.py
    Added four new feature classes: "squareness", average distance to all other points, points per area, and normalized average height value.

--- pointcloud.py
+++ pointcloud.py
@@ -44,73 +44,77 @@ class PointCloud:
         return np.count_nonzero(grid) / (GRID_SIZE * GRID_SIZE)
 
 
-    def kolmogorov_smirnov_xy(self) -> float:
+    def kolmogorov_smirnov_z(self) -> float:
         """Calculate the normality of the z axis of the point cloud using the Kolmogorov-Smirnov test.
         Returns:
             ""A float between 0 and 1. 1 means that the points are normally distributed
         """
-        #firstly calculate the mean of the point cloud
-        xmean = np.mean(self.points[:, 0])
-        ymean = np.mean(self.points[:, 1])
-        #then calculate the distance of each point to this mean
-        distances = np.sqrt((self.points[:, 0] - xmean)**2 + (self.points[:, 1] - ymean)**2)
-        result = stats.kstest(distances, 'norm')
-        return result[0]  # [0] refers to the test statistic, [1] refers to the p-value.
 
-
-    def amount_of_points(self) -> int:
-        """Calculate the amount of points in the point cloud.
+        result = stats.kstest(self.points[:, 2], 'norm')
+        return result[1]  # [1] refers to the p-value.
+    
+    def squareness(self) -> float:
+        """Calculate the squareness of the bounding box of the point cloud.
         Returns:
-            An integer representing the amount of points in the point cloud.
-        """
-        return len(self.points)
+            A float between 0 and 1. 1 means that the bounding box is a perfect
+            square, 0 means it is a line."""
+        x_diff = np.max(self.points[:, 0]) - np.min(self.points[:, 0])
+        y_diff = np.max(self.points[:, 1]) - np.min(self.points[:, 1])
+        squareness_value = np.min([x_diff,y_diff])/np.max([x_diff,y_diff])
+        return squareness_value
+    
+    def avg_distance(self) -> float:
+        """Calculate the average distance from every point of the point cloud
+        to every other point.
+        Returns:
+            A float greater than zero."""
+        total_distance = 0
+        for i in range(len(self.points)):
+            distance = 0
+            for j in range(len(self.points)):
+                x_diff = self.points[i][0] - self.points[j][0]
+                y_diff = self.points[i][1] - self.points[j][1]
+                z_diff = self.points[i][2] - self.points[j][2]
+                diff = np.sqrt(x_diff**2 + y_diff**2 + z_diff**2)
+                distance += diff
+            total_distance += np.average(distance)
+        return np.average(total_distance)
+    
+    def points_per_sqm(self) -> float:
+        """Calculate the average number of points per square meter of the
+        area of the point cloud's bounding box.
+        Returns:
+            A float grearter than zero."""
+        x_min = np.min(self.points[:, 0])
+        x_max = np.max(self.points[:, 0])
+        y_min = np.min(self.points[:, 1])
+        y_max = np.max(self.points[:, 1])
+        quad_points = np.array([[x_min,y_min], [x_max, y_min], [x_min, y_max], [x_max,  y_max]])
+        x_up = np.roll(quad_points[:, 0], -1)
+        y_down = np.roll(quad_points[:, 1], 1)
+        first_sum = np.sum(quad_points[:, 0] * x_up - x_up * quad_points[:, 1])
+        second_sum = np.sum(quad_points[:, 1] * y_down - y_down * quad_points[:, 0])
+        area = np.abs((first_sum + second_sum) / 2)
+        no_of_points = len(self.points)
+        return no_of_points/area
+    
+    def avg_height(self) -> float:
+        """Calculate the average height (z values) of the points of the point
+        cloud.
+        Returns:
+            A float between 0 and 1. 1 means that the average height is equal
+            to the larges height, 0 means it is equal to the lowest.."""
+        z_min = np.min(self.points[:, 2])
+        z_max = np.max(self.points[:, 2])
+        z_range = z_max - z_min
+        z_values = self.points[:, 2]
+        z_avg = np.average(z_values)
+        normalized_avg = (z_avg - z_min)/z_range
+        return normalized_avg
+        
     def plot_horizontal_distribution(self):
         plt.scatter(self.points[:, 0], self.points[:, 1])
         plt.show()
-
-
-    def variance_xy(self) -> float:
-        """Calculate the variance of the horizontal distribution of points.
-        Returns:
-            A float representing the variance of the horizontal distribution of points.
-        """
-        return np.var(self.points[:, :2], axis=0)
-
-    def variance_z(self) -> float:
-        """Calculate the variance of the vertical distribution of points.
-        Returns:
-            A float representing the variance of the vertical distribution of points.
-        """
-        return np.var(self.points[:, 2], axis=0)
-
-
-    def kurtosis_xy(self) -> float:
-        """Calculate the kurtosis of the horizontal distribution of points.
-        Returns:
-            A float representing the kurtosis of the horizontal distribution of points.
-        """
-        return scipy.stats.kurtosis(self.points[:, :2], axis=0, fisher=False, bias=True)
-
-    def kurtosis_z(self) -> float:
-        """Calculate the kurtosis of the vertical distribution of points.
-        Returns:
-            A float representing the kurtosis of the vertical distribution of points.
-        """
-        return scipy.stats.kurtosis(self.points[:, 2], axis=0, fisher=False, bias=True)
-
-    def skew_xy(self) -> float:
-        """Calculate the skewness of the horizontal distribution of points.
-        Returns:
-            A float representing the skewness of the horizontal distribution of points.
-        """
-        return scipy.stats.skew(self.points[:, :2], axis=0, bias=True)
-
-    def skew_z(self) -> float:
-        """Calculate the skewness of the vertical distribution of points.
-        Returns:
-            A float representing the skewness of the vertical distribution of points.
-        """
-        return scipy.stats.skew(self.points[:, 2], axis=0, bias=True)
 
     def plot_3d(self):
         fig = plt.figure()
