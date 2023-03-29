9247cba4a48c7cf8b04622ad5f8b5fcd951c8bbd
Oliver J. Post <65900202+OliverJPost@users.noreply.github.com>
2023-03-29 14:28:57 +0200

    Add grid search for best hyperparameters

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.svm import SVC
 from sklearn.metrics import classification_report, confusion_matrix
 
@@ -15,16 +15,54 @@ def train_svm_linear(train_data, train_labels):
     # Train a linear SVM
     svm_linear = SVC(kernel="linear")
     svm_linear.fit(train_data, train_labels)
+
     return svm_linear
 
 
 def train_svm_poly(train_data, train_labels):
-    svm_poly = SVC(kernel="poly", degree=3)
+    param_grid_poly = {
+        'C': [0.1, 1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1_000_000_000, 10_000_000_000],
+        'degree': [2, 3, 4, 5],
+        'kernel': ['poly']
+    }
+
+    # Create an instance of the SVM classifier
+    svm_poly = SVC()
+
+    # Set up GridSearchCV with cross-validation
+    grid_search_poly = GridSearchCV(svm_poly, param_grid_poly, cv=5, verbose=2, n_jobs=-1)
+
+    # Perform the grid search on the training data
+    grid_search_poly.fit(train_data, train_labels)
+    svm_poly = grid_search_poly.best_estimator_
+    print("Found best parameters for poly kernel:")
+    print(grid_search_poly.best_params_)
     svm_poly.fit(train_data, train_labels)
+
     return svm_poly
 
 
 def train_svm_rbf(train_data, train_labels):
-    svm_rbf = SVC(kernel="rbf")
+    param_grid = {
+        'C': [0.1, 1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1_000_000_000, 10_000_000_000,
+              100_000_000_000],
+        'gamma': [1, 0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001, 0.0000001, 0.00000001, 0.000000001, 0.0000000001,
+                  0.00000000001, 0.000000000001],
+        'kernel': ['rbf']
+    }
+
+    svm_rbf = SVC()
+
+    # Set up GridSearchCV with cross-validation
+    grid_search = GridSearchCV(svm_rbf, param_grid, cv=5, verbose=2, n_jobs=-1)
+
+    # Perform the grid search on the training data
+    grid_search.fit(train_data, train_labels)
+
+    # Print the best hyperparameters
+    print("Best hyperparameters found by grid search:")
+    print(grid_search.best_params_)
+
+    svm_rbf = grid_search.best_estimator_
     svm_rbf.fit(train_data, train_labels)
     return svm_rbf
