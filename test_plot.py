4ceb47ea070e562fe0e466e484238bd7abde4a4b
Oliver J. Post <65900202+OliverJPost@users.noreply.github.com>
2023-03-30 17:16:49 +0200

    Test plot

---
+++ test_plot.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+from sklearn.model_selection import learning_curve
+
+# 1. Import necessary libraries and functions
+
+# 2. Define function to plot learning curve
+def plot_learning_curve(estimator, title, X, y, axes=None, ylim=None, cv=None, n_jobs=None, train_sizes=np.linspace(0.1, 1.0, 5)):
+    if axes is None:
+        _, axes = plt.subplots(1, 1, figsize=(10, 5))
+
+    axes.set_title(title)
+    if ylim is not None:
+        axes.set_ylim(*ylim)
+    axes.set_xlabel("Training examples")
+    axes.set_ylabel("Score")
+
+    train_sizes, train_scores, test_scores = learning_curve(estimator, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes)
+
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+
+    axes.grid()
+    axes.fill_between(train_sizes, train_scores_mean - train_scores_std, train_scores_mean + train_scores_std, alpha=0.1, color="r")
+    axes.fill_between(train_sizes, test_scores_mean - test_scores_std, test_scores_mean + test_scores_std, alpha=0.1, color="g")
+    axes.plot(train_sizes, train_scores_mean, 'o-', color="r", label="Training score")
+    axes.plot(train_sizes, test_scores_mean, 'o-', color="g", label="Cross-validation score")
+    axes.legend(loc="best")
+
+    return plt
+
+# 3. Calculate learning curve
+cv = 5  # Number of cross-validation folds
+train_sizes = np.linspace(0.1, 1.0, 10)  # Change this to control the granularity of the curve
+title = "Learning curve (Random Forest)"
+plot_learning_curve(best_rf, title, X_train, y_train, cv=cv, n_jobs=-1, train_sizes=train_sizes)
+
+# 4. Plot learning curve
+plt.show()
